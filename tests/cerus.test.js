9b3fdc7113f71dfadb5973fbb239c41cfd70ee9e
JortvD <jortvandriel@hotmail.com>
2018-12-14 20:12:59 +0100

    Change most tests

--- tests/cerus.test.js
+++ tests/cerus.test.js
@@ -1,86 +1,28 @@
-var expect = require("chai").expect;
-var cerus = require("../index")();
-var plugins = function() {
+const expect = require("chai").expect;
+const cerus = require("../index")();
+const plugins = () => {
 	cerus.plugins().clear();
 	return cerus.plugins();
 }
 
-describe("cerus", function() {
-	describe("#plugins", function() {
-		describe("constructor", function() {
-			context("with no arguments", function() {
-				it("shouldn't throw any errors", function() {
-					plugins();
-				});
-			});
-		});
-
-		describe("#add", function() {
-			context("with no arguments", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().add();
+describe("cerus", () => {
+	describe("#plugins", () => {
+		describe("#add", () => {
+			context("with an already used name", () => {
+				it("should throw an Error", () => {
+					const func = () => {
+						const _plugins = plugins();
+						_plugins.add({name: "used"});
+						_plugins.add({name: "used"});
 					}
 
 					expect(func).to.throw();
 				});
 			});
 
-			context("with an incorrect first argument", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().add("incorrect");
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with an undefined name", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().add({name: ["incorrect"]});
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with an already used name", function() {
-				it("should throw an Error", function() {
-					var func = function() {
-						var plugins_ = plugins();
-						plugins_.add({name: "used"});
-						plugins_.add({name: "used"});
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with a correct name", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						plugins().add({name: "correct"});
-					}
-
-					expect(func).to.not.throw();
-				});
-			});
-
-			context("with undefined dependencies", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						plugins().add({name: "correct", dependencies: undefined});
-					}
-
-					expect(func).to.not.throw();
-				});
-			});
-
-			context("with a non-existant dependency", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
+			context("with a non-existant dependency", () => {
+				it("should throw a TypeError", () => {
+					const func = () => {
 						plugins().add({name: "correct", dependencies: ["non-existant"]});
 					}
 
@@ -88,89 +30,57 @@ describe("cerus", function() {
 				});
 			});
 
-			context("with a existant dependency", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						var plugins_ = plugins();
-						plugins_.add({name: "existant"});
-						plugins_.add({name: "correct", dependencies: ["existant"]});
+			context("with a existant dependency", () => {
+				it("shouldn't throw any errors", () => {
+					const func = () => {
+						const _plugins = plugins();
+						
+						_plugins.add({name: "existant"});
+						_plugins.add({name: "correct", dependencies: ["existant"]});
 					}
 
 					expect(func).to.not.throw();
 				});
 			});
 
-			context("with non-function entries", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						plugins().add(
-						{
-							name: "correct",
-							extra: "non-function"
-						});
-					}
+			context("with an entry with a non-function value", () => {
+				it("shouldn't throw any errors", () => {
+					plugins().add({
+						name: "correct",
+						extra: "non-function"
+					});
 
-					expect(func).to.not.throw();
+					expect(cerus.extra).to.deep.equal(undefined)
 				});
 			});
 
-			context("with function entries", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						plugins().add(
-						{
-							name: "correct",
-							extra: function() {
-								return "should be added";
-							}
-						});
-					}
+			context("with an entry with the _init and a custom function", () => {
+				it("should add the custom function and call the _init function", () => {
+					const _plugins = plugins();
+					let called = false;
 
-					expect(func).to.not.throw();
-				})
-			});
+					_plugins.add({
+						name: "entry",
+						_init: () => {
+							called = true;
 
-			context("with an _init function entry", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						plugins().add(
-						{
-							name: "correct",
-							_init: function() {
-								return "initializing";
-							}
-						});
-					}
+							return "initializing";
+						},
+						extra: () => {
+							return "should be added";
+						}
+					});
 
-					expect(func).to.not.throw();
+					expect(called).to.equal(true);
+					expect(cerus.extra).to.be.a("function");
 				})
 			});
 		});
 
-		describe("#remove", function() {
-			context("with no arguments", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().remove();
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with an incorrect first argument", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().remove(["incorrect"]);
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with a non-existant plugin", function() {
-				it("should throw an Error", function() {
-					var func = function() {
+		describe("#remove", () => {
+			context("with a non-existant plugin", () => {
+				it("should throw an Error", () => {
+					const func = () => {
 						plugins().remove("non-existant");
 					}
 
@@ -178,119 +88,100 @@ describe("cerus", function() {
 				});
 			});
 
-			context("with an existant plugin", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						var plugins_ = plugins();
-						plugins_.add({name: "existant"});
-						plugins_.remove("existant");
+			context("with an existant plugin", () => {
+				it("shouldn't throw any errors", () => {
+					const func = () => {
+						const _plugins = plugins();
+
+						_plugins.add({name: "existant"});
+						_plugins.remove("existant");
 					}
 
 					expect(func).to.not.throw();
 				});
 			});
 
-			context("with an existant plugin that is depended on", function() {
-				it("should throw an Error", function() {
-					var func = function() {
-						var plugins_ = plugins();
-						plugins_.add({name: "core"});
-						plugins_.add({name: "plugin", dependencies: ["core"]});
-						plugins_.remove("core");
+			context("with an existant plugin that is depended on", () => {
+				it("should throw an Error", () => {
+					const func = () => {
+						const _plugins = plugins();
+
+						_plugins.add({name: "core"});
+						_plugins.add({name: "plugin", dependencies: ["core"]});
+						_plugins.remove("core");
 					}
 
 					expect(func).to.throw();
 				});
 			});
 
-			context("while it has entries", function() {
-				it("shouldn't throw any errors", function() {
-					var func = function() {
-						var plugins_ = plugins();
-						plugins_.add(
-						{
-							name: "existant",
-							extra: function() {
-								return "this is added and then removed";
-							}
-						});
-						plugins_.remove("existant");
-					}
+			context("while it has entries", () => {
+				it("should remove the entry", () => {
+					const _plugins = plugins();
 
-					expect(func).to.not.throw();
+					_plugins.add({
+						name: "existant",
+						extra: () => {
+							return "this is added and then removed";
+						}
+					});
+					_plugins.remove("existant");
+					expect(_plugins.list().length).to.equal(0);
 				});
 			});
 		});
 
-		describe("#has", function() {
-			context("with no arguments", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().has();
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with an incorrect first argument", function() {
-				it("should throw a TypeError", function() {
-					var func = function() {
-						plugins().has(["incorrect"]);
-					}
-
-					expect(func).to.throw();
-				});
-			});
-
-			context("with a non-existant plugin", function() {
-				it("should return false", function() {
+		describe("#has", () => {
+			context("with a non-existant plugin", () => {
+				it("should return false", () => {
 					expect(plugins().has("non-existant")).to.deep.equal(false);
 				});
 			});
 
-			context("with an existant plugin", function() {
-				it("should return true", function() {
-					var plugins_ = plugins();
-					plugins_.add({name: "existant"});
+			context("with an existant plugin", () => {
+				it("should return true", () => {
+					const _plugins = plugins();
 
-					expect(plugins_.has("existant")).to.deep.equal(true);
+					_plugins.add({name: "existant"});
+					expect(_plugins.has("existant")).to.deep.equal(true);
 				});
 			});
 		});
 
-		describe("#list", function() {
-			context("with no plugins", function() {
-				it("should return an empty array", function() {
-					var plugins_ = plugins();
-					expect(plugins_.list()).to.deep.equal([]);
+		describe("#list", () => {
+			context("with no plugins", () => {
+				it("should return an empty array", () => {
+					expect(plugins().list()).to.deep.equal([]);
 				});
 			});
 
-			context("with a single plugin", function() {
-				it("should return an array with the name", function() {
-					var plugins_ = plugins();
-					plugins_.add({name: "plugin1"});
-					expect(plugins_.list()).to.deep.equal(["plugin1"]);
+			context("with a single plugin", () => {
+				it("should return an array with the name", () => {
+					const _plugins = plugins();
+
+					_plugins.add({name: "plugin1"});
+					expect(_plugins.list()).to.deep.equal(["plugin1"]);
 				});
 			});
 
-			context("with multiple plugins", function() {
-				it("should return an array of names", function() {
-					var plugins_ = plugins();
-					plugins_.add({name: "plugin1"});
-					plugins_.add({name: "plugin2"});
-					expect(plugins_.list()).to.deep.equal(["plugin1", "plugin2"]);
+			context("with multiple plugins", () => {
+				it("should return an array of names", () => {
+					const _plugins = plugins();
+
+					_plugins.add({name: "plugin1"});
+					_plugins.add({name: "plugin2"});
+					expect(_plugins.list()).to.deep.equal(["plugin1", "plugin2"]);
 				});
 			});
 		});
 	});
 
-	describe("#use", function() {
-		it("should work the same as .plugins().add()", function() {
-			var plugins_ = plugins();
+	describe("#use", () => {
+		it("should work the same as .plugins().add()", () => {
+			const _plugins = plugins();
+
 			cerus.use({name: "plugin"});
-			expect(plugins_.list().length).to.equal(1);
+			expect(_plugins.list().length).to.equal(1);
 		});	
 	});
 });
